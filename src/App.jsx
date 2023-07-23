13f871cce7fa64003e8cf8a6fcfcdd271e374f4c
Mookambikesan <mookambikesan786@gmail.com>
2023-07-23 12:48:05 +0530

    initial changes

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import Search from './Search'
+import SecondBar from './assets/components/SecondBar'
+import NavBar from './assets/components/NavBar'
+
+const App =() => {
+
+
+  return (
+    <div className='wrapper'>
+      
+      <NavBar/>
+      <Search/>
+
+
+
+
+
+      <SecondBar/>
+
+      </div>
+    
+  )
+
+
+}
+
+export default App
